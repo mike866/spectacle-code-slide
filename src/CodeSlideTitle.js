ddf5763b3e17bf350c3e4add83e320b2e3a3f44b
Michal Chudziak <michal.chudziak@gmail.com>
2017-11-05 13:26:31 +0100

    change colors

--- src/CodeSlideTitle.js
+++ src/CodeSlideTitle.js
@@ -1,23 +1,21 @@
-const React = require('react');
+const React = require("react");
 
 const styles = {
-  position: 'fixed',
-  left: '50%',
-  top: '20px',
-  transform: 'translate(-50%)',
-  padding: '20px 40px',
-  border: '10px solid hotpink',
-  fontSize: '2.5em',
-  color: 'white',
+  position: "fixed",
+  left: "50%",
+  top: "20px",
+  transform: "translate(-50%)",
+  padding: "20px 40px",
+  border: "10px solid #9E9A75",
+  fontSize: "2.5em",
+  color: "#41533B",
   // textTransform: 'uppercase',
-  whiteSpace: 'nowrap'
+  whiteSpace: "nowrap"
 };
 
 class CodeSlideTitle extends React.Component {
   render() {
-    return (
-      <h1 style={styles}>{this.props.children}</h1>
-    );
+    return <h1 style={styles}>{this.props.children}</h1>;
   }
 }
 
